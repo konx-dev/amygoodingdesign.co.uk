889fa4bcbe573484d7ce0370ab0e5291b181934a
Oliver Cox <github@konx.dev>
2023-02-16 23:16:30 +0000

    contact page added

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,5 +1,15 @@
 <template>
     <div>
-        Contact
+        <div>
+            <h1>{{ data['hero-text'].heading }}</h1>
+            <div v-html="data['hero-text'].copy"></div>
+        </div>
+        <div>
+            <a :href="'mailto:' + data.button.title">{{ data.button.title }}</a>
+        </div>
+        <ImagesGallery :ctx="data.gallery" />
     </div>
 </template>
+<script setup>
+import data from '~/content/contact.json'
+</script>
